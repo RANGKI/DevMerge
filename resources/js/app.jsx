4995bf5c449fa8803e4fe64bca158691266bc7b6
RANGKI <marcelrangki@gmail.com>
2025-10-12 19:44:49 +0700

    📌 chore(chore): initialize broadcast tool with laravel reverb

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,25 @@
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
-import '../css/app.css'
+import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
+import '../css/app.css';
+import { configureEcho } from '@laravel/echo-react';
 
 createInertiaApp({
   resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-    return pages[`./Pages/${name}.jsx`]
+    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
+    return pages[`./Pages/${name}.jsx`];
   },
   setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
+    const root = createRoot(el);
+    root.render(<App {...props} />);
+
+    configureEcho({
+      broadcaster: 'reverb',
+      key: import.meta.env.VITE_REVERB_APP_KEY,
+      wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+      wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+      wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+      forceTLS: false,
+      enabledTransports: ['ws', 'wss'],
+    });
   },
-})
+});
